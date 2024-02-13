6a86d116eb7efa64ad19bac636e087fd29b66653
sangeethnavodya <sangeethnavodya33@gmail.com>
2024-02-13 18:50:15 +0530

    Add swagger

--- WashingLaundary/Program.cs
+++ WashingLaundary/Program.cs
@@ -8,11 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("WashingMachineDb")));
 
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
