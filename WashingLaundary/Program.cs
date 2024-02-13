3950ccd857b93fb44ae7e93891134474ee8b4bc6
sangeethnavodya <sangeethnavodya33@gmail.com>
2024-02-13 19:06:01 +0530

    llkkol

--- WashingLaundary/Program.cs
+++ WashingLaundary/Program.cs
@@ -16,7 +16,6 @@ builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer
 
 
 
-
 var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
